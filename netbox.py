9eba2fdea026dc81003da3d0e9451bdc84599643
Erdmann Nils <nils.erdmann@nokia.com>
2026-02-19 11:23:14 +0000

    added support for TLS communication towards Netbox

--- netbox.py
+++ netbox.py
@@ -6,6 +6,8 @@ from typing import Any, Callable, Optional
 from netaddr import IPAddress, IPNetwork
 from pynetbox import api
 from requests import RequestException
+import requests
+import urllib3
 
 NETBOX_URL = os.getenv("NETBOX_URL")
 NETBOX_TOKEN = os.getenv("NETBOX_TOKEN", "")
@@ -14,12 +16,21 @@ NETBOX_SITE = os.getenv("NETBOX_SITE", "default")
 NETBOX_CLUSTER_TYPE = os.getenv("NETBOX_CLUSTER_TYPE", "KVM")
 NETBOX_API_RETRIES = int(os.getenv("NETBOX_API_RETRIES", "3"))
 NETBOX_RETRY_BACKOFF = float(os.getenv("NETBOX_RETRY_BACKOFF", "1.5"))
+NETBOX_CA_CERTS_FILE = os.getenv("NETBOX_CA_CERTS_FILE")
+NETBOX_SSL_VERIFY = os.getenv("NETBOX_SSL_VERIFY", "true").lower() in {"1", "true", "yes", "on"}
 PRIMARY_IPV4_SUBNET = os.getenv("NETBOX_PRIMARY_IPV4_SUBNET", "")
 
 LOGGER = logging.getLogger(__name__)
 nb = api(NETBOX_URL, token=NETBOX_TOKEN) if NETBOX_URL else None
-
-
+if NETBOX_CA_CERTS_FILE is not None:
+    session = requests.Session()
+    session.verify = NETBOX_CA_CERTS_FILE
+    nb.http_session = session
+elif NETBOX_SSL_VERIFY is False:
+    urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+    session = requests.Session()
+    session.verify = False
+    nb.http_session = session
 class NetBoxUnavailableError(RuntimeError):
     """Raised when NetBox is not configured or cannot be reached."""
 
@@ -313,7 +324,7 @@ def create_or_update_vm_interfaces(vm_record: Any, iface: Any) -> Optional[Any]:
             )
             LOGGER.info("Interface '%s' for VM '%s' updated with IP address '%s'.", iface.name, vm_record.name, iface.ip)
         
-        if vm_record.primary_ip is None or IPAddress(ip_obj.address.split('/')[0]) in IPNetwork(PRIMARY_IPV4_SUBNET):
+        if vm_record.primary_ip is None or (PRIMARY_IPV4_SUBNET != "" and IPAddress(ip_obj.address.split('/')[0]) in IPNetwork(PRIMARY_IPV4_SUBNET)):
             _netbox_call(
                 action=f"virtual_machines.update_primary_ip:{vm_record.name}",
                 func=lambda: nb.virtualization.virtual_machines.update([{"id": vm_record.id, "primary_ip4": ip_obj.id}]),
